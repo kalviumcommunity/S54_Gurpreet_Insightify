0d370900d7a754ed5f8bfe60fe21fbd6ce553f9c
Max3796 <gurpreet.singh@kalvium.community>
2024-04-04 16:59:32 +0530

    home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import Navbar from "./Navbar";
+import Image from "../assets/Home Image.png";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Navbar />
+      <div className="home-container">
+        <div className="left">
+          <div className="survey">Survey Maker</div>
+          <div className="bold">
+            Reveal Hidden <br /> Wisdom Through Survey
+          </div>
+          <div className="insight">Insight</div>
+          <div className="tryfree">
+            Create surveys that encourage people to share more with you. Try for
+            free.
+          </div>
+          <div className="create">
+            <button id="create-btn">Create Now</button>
+          </div>
+        </div>
+        <div className="right">
+          <img src={Image} alt="home" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
