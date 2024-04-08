144fa3eac77416d0a14313ea5eb2aea238f48a13
Max3796 <gurpreet.singh@kalvium.community>
2024-04-08 11:52:33 +0530

    Added clerk for auth

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import './Home.css'
+import "./Home.css";
+import Clerk from "./Clerk";
 const Navbar = () => {
   return (
     <div className="navbar">
-        <div className="navbar-left">INSIGHTIFY</div>
-        <div className="navbar-right">
-          <Link to="/discover">Discover</Link>
-          <Link to="/community">Community</Link>
-          <Link to="/about">About Us</Link>
-          <Link to="/testimonials">Testimonials</Link>
-        </div>
+      <div className="navbar-left">INSIGHTIFY</div>
+      <div className="navbar-right">
+        <Link to="/discover">Discover</Link>
+        <Link to="/community">Community</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/testimonials">Testimonials</Link>
+        <Clerk />
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
